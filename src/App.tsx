11dd527db038ae749ff96c96d12c990c2aa0c7e3
David Fernández <davidfernandezcomesana@gmail.com>
2023-05-24 13:59:59 +0200

    terminado

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
+import { useEffect } from 'react';
 import { useCounterStore } from './store/counterStore';
-import shallow from 'zustand/shallow'
+import shallow from 'zustand/shallow';
 
 function App() {
-	const { title, count } = useCounterStore(state => ({
-		count: state.count,
-		title: state.title,
-	}), shallow);
+	const { title, count, posts } = useCounterStore(
+		state => ({
+			count: state.count,
+			title: state.title,
+			posts: state.posts,
+		}),
+		shallow
+	);
+	const { increment, getPosts, clearStore, multiply } = useCounterStore();
+
+	useEffect(() => {
+		getPosts();
+	}, [getPosts]);
 
 	return (
 		<div>
 			<h1>Counter: {count}</h1>
 			<h2>{title}</h2>
+			<button onClick={() => increment(10)}>Incrementar</button>
+			<button onClick={() => clearStore()}>Clear</button>
+			<button onClick={() => multiply(2)}>Multiply</button>
+			<hr />
+
+			{JSON.stringify(posts)}
 		</div>
 	);
 }
